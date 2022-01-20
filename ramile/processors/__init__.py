5c8df46ec2b7fd47c183cf2f6e56aed9926bf7a9
Peak <fujizhong@163.com>
2022-01-20 18:36:44 +0800

    替换模板文件，增加dart支持

--- ramile/processors/__init__.py
+++ ramile/processors/__init__.py
@@ -84,6 +84,7 @@ class FileProcessor(object):
         self.__cache_processor(SwiftProcessor())
         self.__cache_processor(OCProcessor())
         self.__cache_processor(GoProcessor())
+        self.__cache_processor(DartProcessor())
         return
 
     def __get_cached_processor(self, extension):
@@ -106,3 +107,4 @@ from ramile.processors.css_processor import CssProcessor
 from ramile.processors.swift_processor import SwiftProcessor
 from ramile.processors.oc_processor import OCProcessor
 from ramile.processors.go_processor import GoProcessor
+from ramile.processors.dart_processor import DartProcessor
